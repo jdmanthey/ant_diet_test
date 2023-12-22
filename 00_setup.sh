f8b6d4c8e2661ce1c6a9dd01d4c1df8605a92cc0
Joe Manthey <jdmanthey@gmail.com>
2023-12-22 11:18:08 -0600

    Create 00_setup.sh

---
+++ 00_setup.sh
@@ -0,0 +1,11 @@
+#in working directory:
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 02_subset1
+mkdir 03_subset2
+mkdir 04_subset3
+mkdir 05_fasta
+mkdir 10_scripts
+mkdir 10_scripts/bloch_ref
+mkdir 10_scripts/ant_ref
+mkdir 10_scripts/mito_ref
